d6ae54548727aee1e4b7d9aaf3f9ab70b66f3a28
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-12-09 11:25:29 -0800

    changes files to make sure all routes are working, added all handlebars files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,20 @@
-
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-      res.render("index"); 
+    res.render("landing");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/", function(req, res) {
-      res.render("example");
+  app.get("/projects/", function(req, res) {
+    res.render("projects");
+  });
+
+  app.get("/about/", function(req, res) {
+    res.render("about");
+  });
+
+  app.get("/contact/", function(req, res) {
+    res.render("contact");
   });
 
   // Render 404 page for any unmatched routes
